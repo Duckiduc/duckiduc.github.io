f25fbfe42728d93ac6e1b452e6ef1b0c7509545c
Duc-Thomas <duckiduc.dev@gmail.com>
2022-07-30 00:52:51 +0200

    Add Menu component

---
+++ src/components/Menu.svelte
@@ -0,0 +1,39 @@
+<svelte:head>
+  <script src="/scripts/menu.js"></script>
+  <link href="/styles/menu.css" rel="stylesheet" />
+</svelte:head>
+
+<nav>
+  <ul class="box-primary-nav">
+    <li class="box-label">What - When - Where - Who</li>
+    <li>
+      <a href="index.html">Home</a><i class="ion-ios-circle-filled color" />
+    </li>
+    <li><a href="portfolio.html">Portfolio</a></li>
+    <li><a href="about.html">About me</a></li>
+    <li><a href="contact.html">Contact me</a></li>
+    <li><a href="tools.html">Tools / Gear / Softwares I use</a></li>
+    <li>My services are currently unavailable.</li>
+    <li class="box-label">Keep up with my work</li>
+    <li class="box-social">
+      <a href="https://github.com/Duckiduc"><i class="ion-social-github" /></a>
+    </li>
+    <li class="box-social">
+      <a href="https://linkedin.com/in/duc-thomas-nguyen"
+        ><i class="ion-social-linkedin" /></a
+      >
+    </li>
+    <script
+      type="text/javascript"
+      src="https://cdnjs.buymeacoffee.com/1.0.0/button.prod.min.js"
+      data-name="bmc-button"
+      data-slug="ducki"
+      data-color="#FF813F"
+      data-emoji=""
+      data-font="Cookie"
+      data-text="Buy me a coffee"
+      data-outline-color="#000000"
+      data-font-color="#FFFFFF"
+      data-coffee-color="#FFDD00"></script>
+  </ul>
+</nav>
