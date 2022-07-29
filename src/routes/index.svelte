7058d3ed6b080ac9bd6ef7bd08e8e6fda47ad0d0
Duc-Thomas <duckiduc.dev@gmail.com>
2022-07-30 00:53:36 +0200

    Update pages to use new components

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,68 +1,22 @@
 <script>
   import logo from "../assets/img/logo.png";
   import Headline from "../components/HeadlineText.svelte";
-  import Loader from "../components/Loader.svelte"
+  import Loader from "../components/Loader.svelte";
+  import Header from "../components/Header.svelte";
+  import Menu from "../components/Menu.svelte";
 </script>
 
 <svelte:head>
   <script src="/scripts/jquery-2.1.1.js"></script>
   <script src="/scripts/bootstrap.min.js"></script>
-  <script src="/scripts/menu.js"></script>
-  <script src="/scripts/util.js"></script>
-  <script src="/scripts/animated-headline.js"></script>
   <script src="/scripts/isotope.pkgd.min.js"></script>
-  <link href="/styles/animated-headline.css" rel="stylesheet" />
 </svelte:head>
 
 <div>
-  <Loader></Loader>
+  <Loader />
   <div class="container-fluid">
-    <header class="box-header">
-      <div class="box-logo">
-        <a href="index.html"
-          ><img src={logo} width="100" alt="Logo" /></a
-        >
-      </div>
-      <a class="box-primary-nav-trigger" href="#0">
-        <span class="box-menu-text">Menu</span><span class="box-menu-icon" />
-      </a>
-    </header>
-    <nav>
-      <ul class="box-primary-nav">
-        <li class="box-label">What - When - Where - Who</li>
-        <li>
-          <a href="index.html">Home</a><i class="ion-ios-circle-filled color" />
-        </li>
-        <li><a href="portfolio.html">Portfolio</a></li>
-        <li><a href="about.html">About me</a></li>
-        <li><a href="contact.html">Contact me</a></li>
-        <li><a href="tools.html">Tools / Gear / Softwares I use</a></li>
-        <li>My services are currently unavailable.</li>
-        <li class="box-label">Keep up with my work</li>
-        <li class="box-social">
-          <a href="https://github.com/Duckiduc"
-            ><i class="ion-social-github" /></a
-          >
-        </li>
-        <li class="box-social">
-          <a href="https://linkedin.com/in/duc-thomas-nguyen"
-            ><i class="ion-social-linkedin" /></a
-          >
-        </li>
-        <script
-          type="text/javascript"
-          src="https://cdnjs.buymeacoffee.com/1.0.0/button.prod.min.js"
-          data-name="bmc-button"
-          data-slug="ducki"
-          data-color="#FF813F"
-          data-emoji=""
-          data-font="Cookie"
-          data-text="Buy me a coffee"
-          data-outline-color="#000000"
-          data-font-color="#FFFFFF"
-          data-coffee-color="#FFDD00"></script>
-      </ul>
-    </nav>
+    <Header />
+    <Menu />
     <section class="box-intro">
       <div class="table-cell">
         <Headline />
