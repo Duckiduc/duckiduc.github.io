871441b709e87159e9723731605225d6ef9cb197
Duc-Thomas <duckiduc.dev@gmail.com>
2022-12-17 01:36:49 +0100

    Upgrade to SvelteKit version 1

---
+++ src/components/Header.svelte
@@ -0,0 +1,13 @@
+<svelte:head>
+  <script src="/scripts/menu.js"></script>
+  <link href="/styles/menu.css" rel="stylesheet" />
+</svelte:head>
+
+<header class="box-header">
+  <div class="box-logo">
+    <a href="/"><img src="/img/logo.png" width="100" alt="Logo" /></a>
+  </div>
+  <a class="box-primary-nav-trigger" href="#0">
+    <span class="box-menu-text">Menu</span><span class="box-menu-icon" />
+  </a>
+</header>
