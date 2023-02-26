34df982d4afed168cea5a1c1fae2f0716fe46428
Duckiduc <git@ducki.dev>
2023-02-27 00:17:38 +0100

    Add tests for homepage

---
+++ tests/index.spec.ts
@@ -0,0 +1,88 @@
+import { expect, test } from "@playwright/test";
+import projects from "../src/data/projects.json" assert { type: "json" };
+
+test.beforeEach(async ({ page }) => {
+  await page.goto('http://localhost:5173/');
+});
+
+// Navigation tests
+
+test.describe('Navigation', () => {
+  test('should redirect to home page', async ({ page }) => {
+    await page.getByRole('link', { name: 'Menu' }).click();
+    await page.getByRole('link', { name: 'Home' }).click();
+  
+    await expect(page).toHaveURL('http://localhost:5173');
+  });
+  
+  test('should redirect to projects page', async ({ page }) => {
+    await page.getByRole('link', { name: 'Menu' }).click();
+    await page.getByRole('link', { name: 'Portfolio' }).click();
+  
+    await expect(page).toHaveURL('http://localhost:5173/projects');
+  });
+  
+  test('should redirect to about page', async ({ page }) => {
+    await page.getByRole('link', { name: 'Menu' }).click();
+    await page.getByRole('link', { name: 'About Me' }).click();
+  
+    await expect(page).toHaveURL('http://localhost:5173/about');
+  });
+  
+  test('should redirect to contact page', async ({ page }) => {
+    await page.getByRole('link', { name: 'Menu' }).click();
+    await page.getByRole('link', { name: 'Contact Me' }).click();
+  
+    await expect(page).toHaveURL('http://localhost:5173/contact');
+  });
+  
+  test('should redirect to tools page', async ({ page }) => {
+    await page.getByRole('link', { name: 'Menu' }).click();
+    await page.getByRole('link', { name: 'Tools / Gear / Softwares I use' }).click();
+  
+    await expect(page).toHaveURL('http://localhost:5173/tools');
+  });
+});
+
+// Home page tests
+
+test.describe('Home page', () => {
+  test('should contain code.', async ({ page }) => {
+    const title = page.getByText('code.');
+    
+    await expect(title).toHaveText('code.');
+  });
+
+  test('should contain webdesign.', async ({ page }) => {
+    const title = page.getByText('webdesign.');
+
+    await expect(title).toHaveText('webdesign.');
+  });
+
+  test('should contain 3D.', async ({ page }) => {
+    const title = page.getByText('3D.');
+
+    await expect(title).toHaveText('3D.');
+  });
+});
+
+// Home page projects tests
+
+test.describe('Home page projects', () => {
+  projects.forEach(project => {
+    test(`should contain ${project.title} project`, async ({ page }) => {
+      const projectCard = page.locator(`#${project.route}`);
+      // const projectTitle = page.getByText(project.title);
+      await expect(projectCard).toHaveCount(1);
+      // await expect(projectTitle).toHaveText(project.title);
+    });
+  });
+
+  projects.forEach(project => {
+    test(`should redirect to ${project.title} project page`, async ({ page }) => {
+      page.locator(`#${project.route}`).click();
+      
+      await expect(page).toHaveURL(`http://localhost:5173/projects/${project.route}`);
+    });
+  });
+});
