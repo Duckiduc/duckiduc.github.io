b4e44f9fcdf44cf9b5b12ccae204b7687688eed6
Duckiduc <git@ducki.dev>
2023-02-27 00:37:15 +0100

    Add playwright workflow

--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -2,7 +2,7 @@ import { expect, test } from "@playwright/test";
 import projects from "../src/data/projects.json" assert { type: "json" };
 
 test.beforeEach(async ({ page }) => {
-  await page.goto('http://localhost:5173/');
+  await page.goto('http://localhost:4173/');
 });
 
 // Navigation tests
@@ -12,35 +12,35 @@ test.describe('Navigation', () => {
     await page.getByRole('link', { name: 'Menu' }).click();
     await page.getByRole('link', { name: 'Home' }).click();
   
-    await expect(page).toHaveURL('http://localhost:5173');
+    await expect(page).toHaveURL('http://localhost:4173');
   });
   
   test('should redirect to projects page', async ({ page }) => {
     await page.getByRole('link', { name: 'Menu' }).click();
     await page.getByRole('link', { name: 'Portfolio' }).click();
   
-    await expect(page).toHaveURL('http://localhost:5173/projects');
+    await expect(page).toHaveURL('http://localhost:4173/projects');
   });
   
   test('should redirect to about page', async ({ page }) => {
     await page.getByRole('link', { name: 'Menu' }).click();
     await page.getByRole('link', { name: 'About Me' }).click();
   
-    await expect(page).toHaveURL('http://localhost:5173/about');
+    await expect(page).toHaveURL('http://localhost:4173/about');
   });
   
   test('should redirect to contact page', async ({ page }) => {
     await page.getByRole('link', { name: 'Menu' }).click();
     await page.getByRole('link', { name: 'Contact Me' }).click();
   
-    await expect(page).toHaveURL('http://localhost:5173/contact');
+    await expect(page).toHaveURL('http://localhost:4173/contact');
   });
   
   test('should redirect to tools page', async ({ page }) => {
     await page.getByRole('link', { name: 'Menu' }).click();
     await page.getByRole('link', { name: 'Tools / Gear / Softwares I use' }).click();
   
-    await expect(page).toHaveURL('http://localhost:5173/tools');
+    await expect(page).toHaveURL('http://localhost:4173/tools');
   });
 });
 
@@ -82,7 +82,7 @@ test.describe('Home page projects', () => {
     test(`should redirect to ${project.title} project page`, async ({ page }) => {
       page.locator(`#${project.route}`).click();
       
-      await expect(page).toHaveURL(`http://localhost:5173/projects/${project.route}`);
+      await expect(page).toHaveURL(`http://localhost:4173/projects/${project.route}`);
     });
   });
 });
