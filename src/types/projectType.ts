a5516e3563b342bca3322edd15681d3d4a5952f0
Duc-Thomas <duckiduc.dev@gmail.com>
2023-02-26 15:38:57 +0100

    Add project type

---
+++ src/types/projectType.ts
@@ -0,0 +1,31 @@
+import projects from "./data/projects.json";
+
+export type Project = {
+  title: string;
+  route: string;
+  phrase: string;
+  thumbnail: string;
+  image: string;
+  gallery?: {
+    url: string;
+    size?: string;
+  }[];
+  description: string;
+  articles?: {
+    quote: string;
+    title: string;
+    author: string;
+    link: string;
+  }[];
+  links?: {
+    name: string;
+    link: string;
+  }[];
+  tags: {
+    name: string;
+  }[];
+}
+
+export const getProjects = (): Project[] => {
+  return projects;
+}
